7ef3994c62aca446be2e8843f366c88cdf95d4b2
haithamassoli <haitham.b.assoli@gmail.com>
2023-08-18 02:02:05 +0300

    fix category screen style

--- app/(drawer)/(homeStack)/categories/[id].tsx
+++ app/(drawer)/(homeStack)/categories/[id].tsx
@@ -6,7 +6,7 @@ import Colors from "@styles/colors";
 import { Box, ReText } from "@styles/theme";
 import { hs, ms, vs } from "@utils/platform";
 import { Stack, useRouter, useSearchParams } from "expo-router";
-import { Image } from "react-native";
+import { Image, TouchableOpacity } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Category = () => {
@@ -19,60 +19,59 @@ const Category = () => {
   if (isLoading) return <Loading />;
 
   return (
-    <SafeAreaView style={{ flex: 1, marginHorizontal: hs(16) }}>
+    <SafeAreaView style={{ flex: 1 }}>
       <Stack.Screen
         options={{
           title: id,
         }}
       />
-      <Box
-        flexDirection="row"
-        position="absolute"
-        right={0}
-        top={vs(64)}
-        zIndex="modal"
+      <TouchableOpacity
+        onPress={() => router.back()}
+        style={{
+          flexDirection: "row",
+          position: "absolute",
+          right: hs(14),
+          top: vs(72),
+          zIndex: 3,
+        }}
       >
+        <Feather name="chevron-left" size={ms(24)} color={Colors.secondary} />
         <Feather
           name="chevron-left"
           size={ms(24)}
           color={Colors.secondary}
-          onPress={() => router.back()}
           style={{
-            marginRight: hs(-16),
+            marginLeft: hs(-12),
           }}
         />
-        <Feather
-          name="chevron-left"
-          size={ms(24)}
-          color={Colors.secondary}
-          onPress={() => router.back()}
-        />
-      </Box>
+      </TouchableOpacity>
       <Image
         source={require("@assets/images/family.png")}
         style={{ width: "100%", height: vs(274) }}
       />
-      <ReText
-        variant="HeadlineMedium"
-        fontFamily="CairoBold"
-        textAlign="left"
-        color="ternary"
-        marginTop="vs"
-      >
-        المحاضرات التربوية
-      </ReText>
-      <Box gap="vm">
-        {Array.isArray(categories) &&
-          categories[0].videos?.map((video: Video, index: number) => (
-            <VideoButton
-              key={index.toString()}
-              title={`1. ${video.title}`}
-              onPress={() =>
-                router.push(`/videos/${id}?videoTitle=${video.title}`)
-              }
-              duration={video.duration}
-            />
-          ))}
+      <Box marginHorizontal="hm">
+        <ReText
+          variant="HeadlineMedium"
+          fontFamily="CairoBold"
+          textAlign="left"
+          color="ternary"
+          marginTop="vs"
+        >
+          المحاضرات التربوية
+        </ReText>
+        <Box gap="vm">
+          {Array.isArray(categories) &&
+            categories[0].videos?.map((video: Video, index: number) => (
+              <VideoButton
+                key={index.toString()}
+                title={`1. ${video.title}`}
+                onPress={() =>
+                  router.push(`/videos/${id}?videoTitle=${video.title}`)
+                }
+                duration={video.duration}
+              />
+            ))}
+        </Box>
       </Box>
     </SafeAreaView>
   );
